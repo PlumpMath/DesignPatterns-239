f1979829ac6e191b120f2b115ff8819e31776828
taoshengzhou <taoshengzhou@gmail.com>
2015-02-05 17:37:56 +0800

    C#单例模式

---
+++ CSharp/SingletonPattern/FormToolBox.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SingletonPattern
+{
+    public partial class FormToolBox : Form
+    {
+        private static FormToolBox ftb = null;
+        private FormToolBox()
+        {
+            InitializeComponent();
+        }
+        public static FormToolBox GetInstance()
+        {
+            if (ftb == null || ftb.IsDisposed)
+            {
+                ftb = new FormToolBox();
+                ftb.MdiParent = Form1.ActiveForm;
+            }
+            return ftb;
+        }
+
+    }
+}
