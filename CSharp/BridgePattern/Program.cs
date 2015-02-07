c21945fe516436c33ee8a876a66255863f0bed60
taoshengzhou <taoshengzhou@gmail.com>
2015-02-07 19:47:28 +0800

    C#实现桥接模式

---
+++ CSharp/BridgePattern/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgePattern
+{
+    abstract class HandsetSoft
+    {
+        public abstract void Run();
+    }
+    class HandsetGame : HandsetSoft
+    {
+        public override void Run()
+        {
+            Console.WriteLine("运行手机游戏");
+        }
+    }
+    class HandsetAddressList : HandsetSoft
+    {
+        public override void Run()
+        {
+            Console.WriteLine("运行手机通讯录");
+        }
+    }
+    abstract class HandsetBrand
+    {
+        protected HandsetSoft soft;
+        public void SetHandsetSoft(HandsetSoft soft)
+        {
+            this.soft = soft;
+        }
+        public abstract void Run();
+    }
+    class HandsetBrandM : HandsetBrand
+    {
+        public override void Run()
+        {
+            soft.Run();
+        }
+    }
+    class HandsetBrandN : HandsetBrand
+    {
+        public override void Run()
+        {
+            soft.Run();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HandsetBrand ab;
+            ab = new HandsetBrandM();
+            ab.SetHandsetSoft(new HandsetGame());
+            ab.Run();
+        }
+    }
+}
