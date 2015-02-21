613103e0894d6eca3af530496777cd6427d7ae69
taoshengzhou <taoshengzhou@gmail.com>
2015-02-21 21:54:44 +0800

    添加模板模式的C#实现

---
+++ CSharp/TemplatePattern/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplatPattern
+{
+    abstract class AbstractClass
+    {
+        public abstract void PrimitiveOperation1();
+        public abstract void PrimitiveOperation2();
+        public void TemplateMethod()
+        {
+            PrimitiveOperation1();
+            PrimitiveOperation2();
+            Console.WriteLine("");
+        }
+    }
+    class ConcreteclassA : AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("具体类A方法1实现");
+            
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("具体类A方法2实现");
+            
+        }
+    }
+    class ConcreteclassB : AbstractClass
+    {
+        public override void PrimitiveOperation1()
+        {
+            Console.WriteLine("具体类B方法1实现");
+            
+        }
+        public override void PrimitiveOperation2()
+        {
+            Console.WriteLine("具体类B方法2实现");
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass c;
+            c = new ConcreteclassA();
+            c.TemplateMethod();
+
+            c = new ConcreteclassB();
+            c.TemplateMethod();
+
+        }
+    }
+}
